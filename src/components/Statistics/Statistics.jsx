3fd398830ebe63d2767bf60fcdfd034830034996
Anna <72921518+AnnaIzhovska@users.noreply.github.com>
2021-06-29 15:30:13 +0200

    Done

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,12 +1,13 @@
 import {Text} from './Statistics.styles';
+import {CgSmile, CgSmileNone, CgSmileSad } from 'react-icons/cg'
 
 const Statistics = ({good, neutral, bad, total, positivePercentage="100"}) => {
         return(
             <>
             <div>
-            <Text>Good: {good}</Text>
-            <Text>Neutral: {neutral}</Text>
-            <Text>Bad: {bad}</Text>
+            <Text><CgSmile/> Good: {good}</Text>
+            <Text><CgSmileNone/> Neutral: {neutral}</Text>
+            <Text><CgSmileSad/> Bad: {bad}</Text>
             <Text>Total: {total}</Text>
             <Text>Positive feedback: {positivePercentage}%</Text>   
             </div>
