79b71f6ec59badf3443f2b44ef094d8bb76fa1db
Anna <72921518+AnnaIzhovska@users.noreply.github.com>
2021-07-01 12:58:38 +0200

    Перенос в папку App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,78 @@
-import React from'react'
-import Feedback from './components/Feedback/Feedback'
+import React, { Component } from'react'
+import Statistics from './components/Statistics';
+import Section from './components/Section';
+import FeedbackOptions from './components/Feedback/FeedbackOptions';
+import Notification from './components/Notification';
 
-function App() {
-return(
-    <>
-    <Feedback/>
-    </>
-)
+class App extends Component{
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+        vibilde: false
+      }
+
+      show = () => {
+          this.setState({visible: true})
+      }
+
+      hide = () => {
+          this.setState({visible:false})
+      }
+
+      buttonGood = () => {
+        this.setState(prevState =>{
+            return {
+                good: prevState.good + 1,
+            }
+        })
+      }
+      buttonNeutral= () =>{
+         this.setState(prevState =>{
+             return {
+                 neutral: prevState.neutral + 1,
+             }
+         })
+       }
+       buttonBad = () =>{
+         this.setState(prevState =>{
+             return {
+                 bad: prevState.bad + 1,
+             }
+         })
+       }
+
+    render(){
+        const {visible, bad, good, neutral} = this.state;
+        const positivePercentage = `${Math.round((good/(good + bad)) * 100).toFixed(0)}`
+        return(
+            <>
+            <Section title="Please leave feedback"></Section>
+
+            <FeedbackOptions 
+            onButtonGood={this.buttonGood} 
+            onButtonNeutral={this.buttonNeutral} 
+            onButtonBad={this.buttonBad}
+            onShow = {this.show}
+            />
+
+            <Section title="Statictics"></Section>
+
+            {visible &&
+            <Statistics 
+            good={good} 
+            neutral={neutral} 
+            bad={bad} 
+            total={`${good + neutral + bad}`} 
+            positivePercentage={positivePercentage}>
+            </Statistics> 
+            }
+            
+            {!visible &&
+            <Notification message="No feedback given"></Notification>}
+        </>
+        )
+    }
 }
 
 export default App;
